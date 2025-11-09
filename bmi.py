b97e341d0efdaee7cbf0a4399d84b1ca98c2cfd9
Byran <BYRANTHC05.24@ichat.sp.edu.sg>
2025-11-09 18:24:59 +0800

    Lab3_check

--- bmi.py
+++ bmi.py
@@ -5,10 +5,13 @@ def calculate_bmi(height,weight):
     print(bmi)
     if bmi<18.5:
         print("Under weight")
+        return -1
     elif bmi >= 18.5 and bmi <= 25.0:
         print("Normal weight")
+        return 0
     elif bmi>25.0:
         print("over weight")
+        return 1
 
 calculate_bmi(weight=57,height=1.73)
 
